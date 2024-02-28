971903b36276516cf3e1ad0bd34956f1f39ca172
EmelieJohanna <emelie.lundberg42@gmail.com>
2024-02-28 12:33:47 +0100

    added login.js + store config

--- store.js
+++ store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 // import reducers
+import loginReducer from "./redux/login";
 
 export const store = configureStore({
   reducer: {
     // our slices
+    login: loginReducer,
   },
 });
