12c0549dbefd3a8f770377f84c07e395fe45e388
EmelieJohanna <emelie.lundberg42@gmail.com>
2024-02-28 17:02:07 +0100

    added logo + removed whitespace

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-green-600 text-white py-8 mt-10">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto">
         <p>
           &copy; {new Date().getFullYear()} Emelie, Karolina, Josh, Åsa & Ulf.
           All rights reserved ✌️
--- components/Nav.js
+++ components/Nav.js
@@ -1,15 +1,20 @@
 import Link from "next/link";
 import { IoIosMenu } from "react-icons/io";
+import { CiApple } from "react-icons/ci";
 
 const Header = () => {
   return (
     <header className="bg-green-600 text-white py-4 mb-10 whitespace-nowrap">
-      <div className="container mx-auto px-4 flex justify-between items-center">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-3xl ">
+          <CiApple />
+        </div>
         <div className="mb-4 md:mb-0 md:mr-10">
           <Link href="/" className=" text-white no-underline">
             <h1 className="text-2xl font-bold">Apple Quiz</h1>
           </Link>
         </div>
+
         <nav>
           <div className="md:space-x-16 space-x-8 md:text-xl hidden md:flex ">
             <Link
