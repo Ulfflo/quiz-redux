96fa9f087990217cef770a9cd653a0a954856677
EmelieJohanna <emelie.lundberg42@gmail.com>
2024-02-28 17:12:01 +0100

    nav style changes

--- components/Nav.js
+++ components/Nav.js
@@ -4,13 +4,13 @@ import { CiApple } from "react-icons/ci";
 
 const Header = () => {
   return (
-    <header className="bg-green-600 text-white py-4 mb-10 whitespace-nowrap">
+    <header className="bg-green-600 text-green-50 py-4 mb-10 whitespace-nowrap">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-3xl ">
           <CiApple />
         </div>
-        <div className="mb-4 md:mb-0 md:mr-10">
-          <Link href="/" className=" text-white no-underline">
+        <div className="my-4 md:mr-10">
+          <Link href="/" className=" text-green-50 no-underline">
             <h1 className="text-2xl font-bold">Apple Quiz</h1>
           </Link>
         </div>
@@ -30,7 +30,7 @@ const Header = () => {
               Admin
             </Link>
           </div>
-          <button className="md:hidden text-3xl bg-transparent border-none text-white">
+          <button className="md:hidden text-3xl bg-transparent border-none text-green-50">
             <IoIosMenu />
           </button>
         </nav>
