9474b025bf03e5f788f7e529cc39c6f93025aca6
EmelieJohanna <emelie.lundberg42@gmail.com>
2024-02-29 11:03:54 +0100

    added toggle icon

--- components/DarkModeBtn.js
+++ components/DarkModeBtn.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import { toggleDarkMode } from "@/redux/darkMode";
+// import { DarkModeSwitch } from "react-toggle-dark-mode";
 
 function DarkModeButton() {
   const darkMode = useSelector((state) => state.darkMode);
@@ -10,6 +11,7 @@ function DarkModeButton() {
   };
   return (
     <button className={``} onClick={handleToggleDarkMode}>
+      {/* <DarkModeSwitch /> */}
       {darkMode ? "Switch to light mode" : "Switch to dark mode"}
     </button>
   );
