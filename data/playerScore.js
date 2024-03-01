2babb536363e808eab2afc968fbfc6cb5cfb96ad
EmelieJohanna <emelie.lundberg42@gmail.com>
2024-03-01 14:46:29 +0100

    added export to playerScore

--- data/playerScore.js
+++ data/playerScore.js
@@ -1,7 +1,9 @@
-const players = [
+const playerScore = [
   { name: "Bert", score: 7 },
   { name: "Sigrid", score: 2 },
   { name: "Hans", score: 1 },
   { name: "Greta", score: 5 },
   { name: "Kent", score: 10 },
 ];
+
+export default playerScore;
