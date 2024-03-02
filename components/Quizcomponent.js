fb7971fbd20169214cfc47905b09def92ae8d32b
Joshua <joshua.jaup@hotmail.com>
2024-03-02 21:54:14 +0100

    Pushat senaste. Löpande ändringar sker. (funktioner & design)

--- components/Quizcomponent.js
+++ components/Quizcomponent.js
@@ -1,44 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import quizData from '@/data/quizData';
 
-const QuizComponent = ({ quizData, currentQuestionIndex, onNextQuestion }) => {
-  const question = quizData[currentQuestionIndex];
-  const [answered, setAnswered] = useState(false);
+function QuizComponent() {
+  const [currentQuestion, setCurrentQuestion] = useState(0); // State to keep track of the current question
 
-  const handleOptionClick = (optionIndex, isCorrect) => {
-    if (!answered) {
-      setAnswered(true);
-      if (isCorrect) {
-        document.getElementById(`option-${optionIndex}`).classList.add('bg-green-500');
-      } else {
-        document.getElementById(`option-${optionIndex}`).classList.add('bg-red-500');
-      }
-      setTimeout(() => {
-        onNextQuestion();
-      }, 1000);
-    }
+  const handleNextQuestion = () => {
+    setCurrentQuestion(currentQuestion + 1); // Move to the next question
   };
 
   return (
-    <div className="max-w-[600px] my-auto mx-auto bg-green-400 rounded-lg overflow-hidden shadow-lg">
-      <div className="px-10 py-8">
-        <h2 className="text-lg font-semibold">Question {question.id}</h2>
-        <p>{question.question}</p>
-        <div className="mt-4 grid grid-cols-2 gap-4">
-          {question.options.map((option, index) => (
-            <button
-              key={index}
-              id={`option-${index}`}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              onClick={() => handleOptionClick(index, option.isCorrect)}
-            >
-              {option.answer}
-            </button>
-          ))}
+    <div className="container mx-auto p-4">
+      <div className="flex justify-center items-center h-full">
+        <div className="bg-green-500 w-9/12 h-3/5 rounded-lg">
+          <h1 className="text-white text-center text-xl font-bold p-6 border-b-4 border-slate-300">
+            Question {quizData[currentQuestion].id}: {quizData[currentQuestion].question}
+          </h1>
+          <div className="grid grid-cols-2 gap-4 justify-center p-8">
+            {quizData[currentQuestion].options.map((option, index) => (
+              <button
+                key={index}
+                className="bg-green-400 text-white font-semibold px-6 py-3 rounded-md hover:bg-green-600 transition-colors"
+                onClick={handleNextQuestion}
+              >
+                {option.answer}
+              </button>
+            ))}
+          </div>
         </div>
-        <p className="mt-4">ID: {question.id}</p>
       </div>
     </div>
   );
-};
+}
 
 export default QuizComponent;
