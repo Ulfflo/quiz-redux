6fc11368451df8e590740017f81b36a328113f3a
EmelieJohanna <emelie.lundberg42@gmail.com>
2024-03-03 15:35:07 +0100

    added Layout to quizpage + changed QuizComponent file name

--- components/Quizcomponent.js
+++ components/Quizcomponent.js
@@ -1,26 +1,36 @@
 import React, { useState } from "react";
-import quizData from '@/data/quizData';
+import quizData from "@/data/quizData";
+import { useSelector } from "react-redux";
 
 function QuizComponent() {
-  const [currentQuestion, setCurrentQuestion] = useState(0); 
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
   const handleNextQuestion = () => {
-    setCurrentQuestion(currentQuestion + 1); 
+    setCurrentQuestion(currentQuestion + 1);
   };
+
+  const darkMode = useSelector((state) => state.darkMode);
 
   return (
     <div className="container mx-auto p-4">
       <div className="flex justify-center items-center h-full">
-        <div className="bg-green-500 w-10/12 h-4/5 rounded-lg">
+        <div
+          className={` ${
+            darkMode ? "bg-sky-950" : "bg-green-500"
+          } w-10/12 h-4/5 rounded-lg`}
+        >
           <h1 className="text-white text-center text-xl font-bold p-6 border-b-4 border-slate-300">
-            Question {quizData[currentQuestion].id}: {quizData[currentQuestion].question}
+            Question {quizData[currentQuestion].id}:{" "}
+            {quizData[currentQuestion].question}
           </h1>
           <div className="grid grid-cols-2 gap-4 justify-center p-8">
             {quizData[currentQuestion].options.map((option, index) => (
               <button
                 key={index}
-                className="bg-green-400 text-white font-semibold px-6 py-3 rounded-md w-full"
-                style={{ minWidth: '150px' }} 
+                className={` ${
+                  darkMode ? "bg-sky-800" : "bg-green-500 border-green-800"
+                } text-white font-semibold px-6 py-3 rounded-md w-full border-solid shadow-xl`}
+                style={{ minWidth: "150px" }}
                 onClick={handleNextQuestion}
               >
                 {option.answer}
@@ -34,4 +44,3 @@ function QuizComponent() {
 }
 
 export default QuizComponent;
-
--- pages/quizpage.js
+++ pages/quizpage.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import QuizComponent from '@/components/QuizComponent';
-import quizData from '@/data/quizData';
+import React from "react";
+import QuizComponent from "@/components/QuizComponent";
+import quizData from "@/data/quizData";
+import Layout from "@/components/Layout";
 
 const Quizpage = () => {
   return (
-    <div className='container mx-auto bg-slate-50'>
-        <h1 className='text-center p-4'>Question...</h1>
+    <Layout>
+      <h1 className="text-center p-4">Question...</h1>
       <QuizComponent />
-    </div>
+    </Layout>
   );
 };
 
