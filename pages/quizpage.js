851a3496e53cd1ceb60fe629990f4bc0b57d64c6
Joshua <joshua.jaup@hotmail.com>
2024-03-04 19:31:39 +0100

    - Question rubrik

--- pages/quizpage.js
+++ pages/quizpage.js
@@ -6,7 +6,6 @@ import Layout from "@/components/Layout";
 const Quizpage = () => {
   return (
     <Layout>
-      <h1 className="text-center p-4">Question...</h1>
       <QuizComponent />
     </Layout>
   );
